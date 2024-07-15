d47b9557b59bbc18cb3e999088d2f66e85a16167
bleemesser <becleem@nuevaschool.org>
2024-07-15 00:56:28 -0700

    Mon Jul 15 00:56:28 PDT 2024

--- first_time_setup.sh
+++ first_time_setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Install Git and Nginx
 sudo apt update
--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 # if --stop is passed, stop the service otherwise start it
 if [ "$1" == "--stop" ]; then
-#   sudo systemctl stop pocketbase.service
-    echo "Stopping service..."
+  echo "Stopping service..."
+  sudo systemctl stop pocketbase.service
 elif [ "$1" == "--start" ]; then
     echo "Starting service..."
-#   sudo systemctl start pocketbase.service
+  sudo systemctl start pocketbase.service
 fi
 
 # if no arguments are passed, print the status of the service and a usage message
 if [ "$1" == "" ]; then
   echo "Usage: ./run.sh [--start | --stop]"
-#   sudo systemctl status pocketbase.service
+  sudo systemctl status pocketbase.service
 fi
