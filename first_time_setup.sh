50208a94f622410134096b682d23afda0391efb9
bleemesser <becleem@nuevaschool.org>
2024-07-15 00:23:16 -0700

    setup scripts

---
+++ first_time_setup.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Install Git and Nginx
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y nginx
+
+# Copy nginx.conf to /etc/nginx/nginx.conf and reload Nginx
+sudo useradd nginx
+sudo cp nginx.conf /etc/nginx/nginx.conf
+sudo systemctl restart nginx
+sudo nginx -s reload
+
+# Create a systemd service file to run the server
+cat << EOF | sudo tee /etc/systemd/system/pocketbase.service > /dev/null
+[Unit]
+Description=Pocketbase Service
+After=network.target
+
+[Service]
+User=$USER
+Group=$(id -gn)
+WorkingDirectory=$(pwd)
+ExecStart=$(pwd)/main serve --http "127.0.0.1:8090"
+Restart=unless-stopped
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload systemd
+sudo systemctl daemon-reload
+
+# Enable and start the service
+sudo systemctl enable pocketbase.service
+sudo systemctl restart pocketbase.service
